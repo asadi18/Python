34fd4fdb3c2a3d740adb5c86d12d20c16cfdfcc6
Asad Ishteaque <108184512+asadi18@users.noreply.github.com>
2024-02-17 11:11:00 +0600

    Basic Codes

---
+++ 1. Python-Basics/hi.py
@@ -0,0 +1,30 @@
+#print ('Hi I am running');
+
+###This is comment line, so I am writing variables bellow
+
+###Simple add operation
+
+# a = 5
+# b = 8 
+# print(a+b)
+
+# c = 9.6
+# d = 4.5
+# print(c+d)
+
+
+
+###Type Casting
+
+# firstNum = 7
+# secondNum = input ("Enter a number: ")
+
+# print("sum: ", firstNum + int(secondNum)) ## We have to take int for ty,ll be a string because paythong takes input as string 
+
+
+###Operators --- 3 types of Operators 1.Arithmetic(+,-,*,/)  2. Comparator: <,>,<=,>=  3. logical: and, or, not
+
+val1 = input("Enter the first value: ")
+val2 = input ("Enter the second value: ")
+
+print(int(val1)*int(val2))  # we can do all operaions here
